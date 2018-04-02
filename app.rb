675a02e3ca4df077781c924a22c7954dece4e56c
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-02 16:30:34 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -21,7 +21,10 @@ class App < Sinatra::Base
   get '/multiply/:num1/:num2' do
     @num1 = params[:num1]
     @num2 = params[:num2]
-    "#{@num1 * @num2}"
+    @result = @num1 * @num2
+
+    erb :result
+    
   end
   # Code your final two routes here:
 
